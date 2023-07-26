9eda97674d2bd5f4cdd10f8037e2486f7260174d
Levitt <tlevitt@wpi.edu>
2023-07-26 15:00:07 -0400

    wednesday afternoon

--- numgame/numgame.js
+++ numgame/numgame.js
@@ -1,18 +1,38 @@
 const guess = document.getElementById("guess");
 const report = document.getElementById("report");
+let body = document.body;
 
-const MAXNUM = 100000;
+const MAXNUM = 1000000;
 let secret;
 
 function loadGame(){
     guess.max = MAXNUM;
-    secret = Math.floor(Math.random() * MAXNUM);
-    report.innerHTML = secret;
+    secret = Math.floor(Math.random() * (MAXNUM + 1));
+    
 
 }
 
 function makeGuess(){
 
+    let myGuess = guess.value;
 
+    if (myGuess < secret){
+
+        report.innerHTML += `<br/>[${myGuess}] is too low!`;
+
+    } else if (myGuess > secret){
+
+        report.innerHTML += `<br/>[${myGuess}] is too high!`;
+
+    } else if (myGuess == secret) {
+
+        report.innerHTML += `<br/>You win!`;
+        
+
+    } else {
+
+        report.innerHTML = `<br/>Something went wrong.`;
+
+    }
 
 }
