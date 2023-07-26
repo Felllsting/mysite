49432f80fd282b2b71c9864f707cbeb7639b98d3
Levitt <tlevitt@wpi.edu>
2023-07-26 10:26:24 -0400

    started numgame wednesday morning

---
+++ numgame/numgame.js
@@ -0,0 +1,18 @@
+const guess = document.getElementById("guess");
+const report = document.getElementById("report");
+
+const MAXNUM = 100000;
+let secret;
+
+function loadGame(){
+    guess.max = MAXNUM;
+    secret = Math.floor(Math.random() * MAXNUM);
+    report.innerHTML = secret;
+
+}
+
+function makeGuess(){
+
+
+
+}
